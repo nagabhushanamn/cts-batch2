03810a9e6f7df3eabd4db014c228ed894c268467
nagabhushanamn <nagabhushanamn@gmail.com>
2016-05-19 17:46:42 +0530

    day 3 commit

---
+++ shop_v2/js/app.js
@@ -0,0 +1,60 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+(function() {
+
+	var storeModule = angular.module('store', []);
+
+	// --------------------------------------------
+	// Model ( load from server ) ( data-model )
+	var items = [ {
+		name : 'Laptop',
+		price : 198000, // number
+		description : 'New Mac Pro',
+		canBuy : true,
+		notAvailable : false,
+		date : Date.now(), // timestamp
+		discount : 10000
+	}, {
+		name : 'Mobile',
+		price : 8000,
+		description : 'New model',
+		canBuy : true,
+		notAvailable : false,
+		date : Date.now()
+	// timestamp
+	} ];
+	// --------------------------------------------
+
+	// NG - controller
+	storeModule.controller('StoreController', function($scope,$filter) {
+		// $scope.product = item; // $scope --> ViewModel
+		
+		//$scope.items = items;
+		
+		console.log($filter('uppercase')('nag'));
+		console.log($filter('priceDiscount')(1000,100));
+		
+		$scope.items=$filter('orderBy')(items,'price',true);
+		
+		
+	});
+
+	// --------------------------------------------
+
+	// Filter
+	storeModule.filter('priceDiscount', function() {
+		return function(originalPrice, discount) {
+			if (discount) {
+				return originalPrice - discount;
+			} else {
+				return originalPrice - 1;
+			}
+		};
+	});
+	
+	// --------------------------------------------
+
+
+})();
